1222a942e767513a44a2ece61443d8ae5108be06
Filipe <2b.filipelourenco@gmail.com>
2019-01-30 19:37:31 -0200

    Update 'final_project.py' - pycodestyle
    Only with one 'error' that i need to keep.

--- final_project.py
+++ final_project.py
@@ -1,6 +1,7 @@
 from random import randint
 from time import sleep
 
+
 class Player():
     acoes_validas = ['pedra', 'papel', 'tesoura']
 
@@ -12,12 +13,15 @@ class Player():
     def play(self):
         return 'pedra'
 
-    def learn(self, my_move): # Se eu colocar o 'last_oponnent_move' aqui, o aplicativo dá 'crash'.
+    # Se eu colocar o 'last_oponnent_move' aqui, o aplicativo dá 'crash'.
+    def learn(self, my_move):
         self.my_move = my_move
+
 
 class RandomPlayer(Player):
     def play(self):
         return self.acoes_validas[randint(0, 2)]
+
 
 class HumanPlayer(Player):
     def play(self):
@@ -26,9 +30,10 @@ class HumanPlayer(Player):
             player_move = input(' Jogada digitada inválida. Tente digitar uma dessas jogadas: pedra, papel ou tesoura!\n ')
         return player_move
 
+
 class CyclePlayer(Player):
     def play(self):
-        if self.my_move == None:
+        if self.my_move is None:
             return 'pedra'
         elif self.my_move == 'pedra':
             return 'papel'
@@ -36,6 +41,7 @@ class CyclePlayer(Player):
             return 'tesoura'
         else:
             return 'pedra'
+
 
 class ReflectPlayer(Player):
     def play(self):
@@ -45,33 +51,34 @@ class ReflectPlayer(Player):
 
     def learn(self, last_opponent_move):
         self.last_opponent_move = last_opponent_move
-        
+
+
 class Game():
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
-    
+
     def play_match(self):
         print('\n PEDRA, PAPEL ou TESOURA ~ O jogo começou!')
         rounds = int(input(' > > > Digite o número de partidas que você quer jogar:\n '))
-        
+
         for partida in range(0, rounds):
-            print(f'\n \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/')
-            print(f" \/\/\/\/\/\/ PARTIDA DE Nº: {partida} \/\/\/\/\/\/")
+            print(f'\n ****************************************************************************')
+            print(f' ***************************** PARTIDA DE Nº: {partida} *****************************')
             self.play_round()
 
         if self.player1.placar > self.player2.placar:
             print('\n Parabéns! O jogador Nº1 ganhou o jogo.')
-            print(f' ************************************************************************************')
+            print(f' ****************************************************************************')
         elif self.player1.placar > self.player2.placar:
             print('\n Parabéns! O jogador Nº2 ganhou o jogo.!')
-            print(f' ************************************************************************************')
+            print(f' ****************************************************************************')
         else:
             print('\n Ops! O jogo ficou empatado ... ;) ...')
-            print(f' ************************************************************************************')
+            print(f' ****************************************************************************')
 
         print(f'\n O placar final do jogo ficou em: {self.player1.placar} --||-- {self.player2.placar}')
-        print(" > > > O jogo acabou! Essa janela será fechada em 15 segundos.")
+        print(' > > > O jogo acabou! Essa janela será fechada em 15 segundos.')
         sleep(15)
 
     def play_round(self):
@@ -82,41 +89,43 @@ class Game():
         self.player2.learn(player1_jogada)
 
         if ((player1_jogada == 'pedra' and player2_jogada == 'tesoura') or
-            (player1_jogada == 'tesoura' and player2_jogada == 'papel') or
-            (player1_jogada == 'papel' and player2_jogada == 'pedra')):
+                (player1_jogada == 'tesoura' and player2_jogada == 'papel') or
+                (player1_jogada == 'papel' and player2_jogada == 'pedra')):
             self.player1.placar += 1
-            print(f' ____________________________________________________________________________________')
+            print(f' ****************************************************************************')
             print('\n Jogador Nº 1 ganhou a partida!')
             print(f' O placar do jogo está: {self.player1.placar} --||-- {self.player2.placar}')
             print(f' O jogador Nº1 jogou "{player1_jogada}" e o jogador Nº2 jogou "{player2_jogada}".')
 
         elif ((player2_jogada == 'pedra' and player1_jogada == 'tesoura') or
-              (player2_jogada == 'tesoura' and player1_jogada == 'papel') or
-              (player2_jogada == 'papel' and player1_jogada == 'pedra')):
+                (player2_jogada == 'tesoura' and player1_jogada == 'papel') or
+                (player2_jogada == 'papel' and player1_jogada == 'pedra')):
             self.player2.placar += 1
-            print(f' ____________________________________________________________________________________')
+            print(f' ****************************************************************************')
             print('\n Jogador Nº 2 ganhou a partida!')
             print(f' O placar do jogo está: {self.player1.placar} --||-- {self.player2.placar}')
             print(f' O jogador Nº1 jogou "{player1_jogada}" e o jogador Nº2 jogou "{player2_jogada}".')
 
         else:
-            print(f' ____________________________________________________________________________________')
+            print(f' ****************************************************************************')
             print('\n A partida está empatada!')
             print(f' O placar do jogo está: {self.player1.placar} --||-- {self.player2.placar}')
             print(f' Ambos fizeram a mesma jogada. O jogador Nº1 jogou "{player1_jogada}" e o jogador Nº2 jogou "{player2_jogada}".')
 
+
 game = Game(RandomPlayer(), HumanPlayer())
 game.play_match()
 
-#chamada_escolhas = ' [1]RandomPlayer, [2]HumanPlayer, [3]ReflectPlayer, [4]CyclePlayer'
+# chamada_escolhas = ' [1]RandomPlayer, [2]HumanPlayer, [3]ReflectPlayer, [4]CyclePlayer'
 
-#if __name__ == '__main__':
-    #jogadores_disponiveis = {1: RandomPlayer(), 2: HumanPlayer(), 3: ReflectPlayer(), 4: CyclePlayer()}
-    #print (chamada_escolhas)
-    #print(f' ************************************************************************************\n')
+# if __name__ == '__main__':
+# jogadores_disponiveis = {1: RandomPlayer(), 2: HumanPlayer(), 3: ReflectPlayer(), 4: CyclePlayer()}
+# print (chamada_escolhas)
+# print(f' ************************************************************************************\n')
 
-    #e1 = input(" Digite o número correspondente ao jogador Nº1: ")
-    #e2 = input(" Digite o número correspondente ao jogador Nº2: ")
+# e1 = input(" Digite o número correspondente ao jogador Nº1: ")
+# e2 = input(" Digite o número correspondente ao jogador Nº2: ")
 
-    #game = Game(jogadores_disponiveis[e1], jogadores_disponiveis[e2])
-    #game.play_match()
+# game = Game(jogadores_disponiveis[e1], jogadores_disponiveis[e2])
+# game.play_match()
+
